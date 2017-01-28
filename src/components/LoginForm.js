91fce3020c26feffc931db2fdc9139fd7a1fb030
Hebert Porto <hebertporto@gmail.com>
2017-01-27 22:34:50 -0200

    criando input
    criando input

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 class LoginForm extends Component {
+  state = { text: '' };
+
    render() {
      return (
         <Card>
           <CardSection>
-            <TextInput style={{ height: 20, width: 100 }} />
+            <Input
+              value={this.state.text}
+              onChangeText={text => this.setState({ text })}
+              style={{ height: 50, width: 200 }}
+            />
           </CardSection>
 
           <CardSection />
